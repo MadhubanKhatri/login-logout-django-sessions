a5d621ae0946e33ec11356e115b8dbdb1508486c
Madhuban Khatri <madhuban211998@gmail.com>
2020-12-04 12:02:27 +0800

    Create models.py

---
+++ models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+class User(models.Model):
+	username = models.CharField(max_length=30)
+	password = models.CharField(max_length=50)
+
+	def __str__(self):
+		return self.username
+    
